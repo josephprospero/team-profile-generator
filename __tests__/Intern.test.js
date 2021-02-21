2c62bdf10a8db89672660221888d697de63aa158
Joseph Prospero <jprospero413@gmail.com>
2021-02-20 20:40:38 -0800

    tested new intern class with jest

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,39 @@
 const Intern = require('../lib/Intern.js');
+
+test('Create Intern object', () => {
+    const intern = new Intern('Ryan', 111, 'ryan@gmail.com', "St. Marque's");
+
+    expect(intern.name).toBe('Ryan');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toBe('ryan@gmail.com')
+});
+
+test('Get Intern name', () => {
+    const intern = new Intern('Ryan', 111, 'ryan@gmail.com', "St. Marque's");
+
+    expect(intern.getName()).toEqual(expect.any(String));
+});
+
+test('Get Intern ID', () => {
+    const intern = new Intern('Ryan', 111, 'ryan@gmail.com', "St. Marque's");
+
+    expect(intern.getId()).toEqual(expect.any(Number))
+});
+
+test('Get Intern email', () => {
+    const intern = new Intern('Ryan', 111, 'ryan@gmail.com', "St. Marque's");
+
+    expect(intern.getEmail()).toEqual(expect.any(String));
+});
+
+test('Get Intern School', () => {
+    const intern = new Intern('Ryan', 111, 'ryan@gmail.com', "St. Marque's");
+
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+
+test('Get Intern role', () => {
+    const intern = new Intern('Ryan', 111, 'ryan@gmail.com', "St. Marque's")
+
+    expect(intern.getRole()).toEqual(expect.any(String));
+});
